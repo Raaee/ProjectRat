009081c0dda1bb49cd1cb19e03e0cf5f85e3dd91
Enzo Falone <enzofalone123@gmail.com>
2025-02-19 17:08:49 -0500

    fixed formatting for private variables

--- BuildSpring2025_ProjectRat/Assets/Scripts/Input/PlayerMovement.cs
+++ BuildSpring2025_ProjectRat/Assets/Scripts/Input/PlayerMovement.cs
@@ -5,32 +5,32 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 10f;
     public float smoothTime = 0.1f;
 
-    private PlayerControl _playerControl;
-    private Rigidbody2D _rb;
-    private Vector2 _moveDirection = Vector2.zero;
-    private Vector2 _currentVelocity = Vector2.zero;
-    private Vector2 _targetPos = Vector2.zero;
+    private PlayerControl playerControl;
+    private Rigidbody2D rb;
+    private Vector2 moveDirection = Vector2.zero;
+    private Vector2 currentVelocity = Vector2.zero;
+    private Vector2 targetPos = Vector2.zero;
     
     void Awake() {
-        _playerControl = new PlayerControl();
-        _rb = GetComponent<Rigidbody2D>();
-        if (_rb is null) {
+        playerControl = new PlayerControl();
+        rb = GetComponent<Rigidbody2D>();
+        if (rb is null) {
             Debug.LogError("Rigidbody2D component is missing");
         }
     }
 
     private void OnEnable() {
-        _playerControl.Enable();
+        playerControl.Enable();
     }
 
     private void OnDisable() {
-        _playerControl.Disable();
+        playerControl.Disable();
     }
 
     void FixedUpdate() {
-        _moveDirection = _playerControl.Player.Movement.ReadValue<Vector2>().normalized;
-        _targetPos = Vector2.SmoothDamp(_targetPos, _moveDirection, ref _currentVelocity, smoothTime);
+        moveDirection = playerControl.Player.Movement.ReadValue<Vector2>().normalized;
+        targetPos = Vector2.SmoothDamp(targetPos, moveDirection, ref currentVelocity, smoothTime);
 
-        _rb.velocity = _targetPos * moveSpeed;
+       rb.velocity = targetPos * moveSpeed;
     }
 }
