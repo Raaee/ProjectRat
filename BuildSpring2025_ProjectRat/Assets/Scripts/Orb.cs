78688e12b76d9dec6a84cf98f86e32aeae25d4c1
Raaee <99899402+Raaee@users.noreply.github.com>
2025-02-28 23:55:33 -0500

    acid orb visuals

--- BuildSpring2025_ProjectRat/Assets/Scripts/Orb.cs
+++ BuildSpring2025_ProjectRat/Assets/Scripts/Orb.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Orb : Collectable
 {
+    private SpriteRenderer sr;
+    public Sprite[] orbSprites;
+    private void Start() {
+        sr = GetComponentInChildren<SpriteRenderer>();
+        ChooseRandomVisual();
+    }
+    private void ChooseRandomVisual() {
+        int rand = Random.Range(0, orbSprites.Length-1);
+        sr.sprite = orbSprites[rand];
+    }
     public override void Collect() {
         FindObjectOfType<AcidOrbs>().AddOrbs(1);
     }
