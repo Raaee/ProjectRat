3f0fe0e07cab619fd09d7b6415fcf95ca240db44
Raaee <99899402+Raaee@users.noreply.github.com>
2025-02-28 19:10:04 -0500

    adjusted aoe special, added attack ps

--- BuildSpring2025_ProjectRat/Assets/Scripts/Enemy/Attacks/AoESpecial.cs
+++ BuildSpring2025_ProjectRat/Assets/Scripts/Enemy/Attacks/AoESpecial.cs
@@ -5,15 +5,9 @@ using UnityEngine;
 
 public class AoESpecial : MonoBehaviour
 {
-    [SerializeField] private float spawnRadius = 10f;
-    [SerializeField] private float attackRadius = 5f;
-    [SerializeField] private int attackDamage = 5;
+    [field: SerializeField] public float spawnRadius { get; private set; } = 10f;
     [SerializeField] private float delayPerSpawn = 0.2f;
-    [SerializeField] private float damageDelay = 0.5f;
-    public List<GameObject> collisions;
-
-    public GameObject areaPrefab;
-    public List<GameObject> spawnedAreas;
+    public GameObject attackPrefab;
 
     [Header("SPAWN AREAS")]
     [SerializeField][Range(1, 10)] private int maxAmtPerAttack = 1;
@@ -27,51 +21,25 @@ public class AoESpecial : MonoBehaviour
     }
     private IEnumerator SpawnAreas() {
         int amtAreas = maxAmtPerAttack;
-        collisions.Clear(); // resets lists of collisions
-        RemoveOldAreas(); // clears old area game objects from scene
 
         if (randomAmtPerAttack) {
             amtAreas = Random.Range(1, maxAmtPerAttack);
         }
+
         int areaCounter = 0;
         while (areaCounter < amtAreas) {
 
             randCenterPoint = new Vector2(Random.Range(transform.position.x - spawnRadius, transform.position.x + spawnRadius),
             Random.Range(transform.position.y - spawnRadius, transform.position.y + spawnRadius)); // random point within a square area
 
-            Collider2D[] hitObjects = Physics2D.OverlapCircleAll(randCenterPoint, attackRadius);
-            foreach (Collider2D hit in hitObjects) {
-                collisions.Add(hit.gameObject);
-            }
-            GameObject areaGO = Instantiate(areaPrefab, randCenterPoint, Quaternion.identity);
-            spawnedAreas.Add(areaGO);
-            // spawning area should auto-start animations
+            GameObject areaGO = Instantiate(attackPrefab, randCenterPoint, Quaternion.identity);
+            AoEAttack attack = areaGO.GetComponent<AoEAttack>();
+            attack.spawnRadius = spawnRadius;
+            attack.randCenterPoint = randCenterPoint;
+            attack.StartAttack();
+
             yield return new WaitForSeconds(delayPerSpawn);
             areaCounter++;
         }
-        // trigger damage animations here
-        yield return new WaitForSeconds(damageDelay);
-        DamageAllCollisions();
-    }
-    private void DamageAllCollisions() {
-        foreach (GameObject go in collisions) {
-            if (go.tag == "Player") {
-                go.GetComponent<Health>().RemoveHealth(attackDamage);
-            }
-            if (go.tag == "MinionRat") {
-                //go.GetComponent<AggroStatus>().isAggro = false; // this where the purification happens
-                Debug.Log("Purified", gameObject);
-            }
-        }
-    }
-    private void RemoveOldAreas() {
-        foreach (GameObject go in spawnedAreas) {
-            Destroy(go);
-        }
-        spawnedAreas.Clear();
-    }
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(randCenterPoint, attackRadius);
     }
 }
