184a8a8cb4a5f8f36d677b0f4df387d84e121e8d
Raaee <99899402+Raaee@users.noreply.github.com>
2025-03-02 15:01:03 -0500

    acid orb animations
    - hover bounce
    - particles
    - pickup visual

--- BuildSpring2025_ProjectRat/Assets/Scripts/Visuals/HoverBounce.cs
+++ BuildSpring2025_ProjectRat/Assets/Scripts/Visuals/HoverBounce.cs
@@ -6,15 +6,32 @@ using UnityEngine;
 public class HoverBounce : MonoBehaviour
 {
     [SerializeField] private GameObject objToMove;
-    [SerializeField] private float duration = 10f;
-    [SerializeField] private float hoverStrength = 1f;
+    [SerializeField] private float duration = 2f;
+    [SerializeField] private float maxHoverHeight = 1f;
     [SerializeField] private Ease easeType = Ease.Linear;
-    [SerializeField] private bool smoothSnap = true;
-
+    private Tween tween;
     private void Start() {
         StartHover();
     }
     public void StartHover() {
-        objToMove.transform.DOMove(Vector3.up, duration, smoothSnap);
+        StartCoroutine(Hover());     
+    }
+    private IEnumerator Hover() {
+        Vector3 startPos = transform.position;
+        do {
+            float hoverHeight = Random.Range(maxHoverHeight/2, maxHoverHeight);
+            Vector3 endPoint = new Vector3(startPos.x, startPos.y + hoverHeight, 0);
+            tween = objToMove.transform.DOMove(endPoint, duration).SetEase(easeType);
+            yield return new WaitUntil(IsTweenComplete);
+            tween = objToMove.transform.DOMove(startPos, duration).SetEase(easeType);
+            yield return new WaitUntil(IsTweenComplete);
+        }
+        while (IsTweenComplete());
+    }
+    private bool IsTweenComplete() {
+        return tween.IsActive() == false;
+    }
+    public void KillHover() {
+        tween.Complete();
     }
 }
