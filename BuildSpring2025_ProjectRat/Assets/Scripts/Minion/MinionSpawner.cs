398973629dbad59f841675ddff0099967b3188f9
Raaee <varangurenv@gmail.com>
2025-03-04 13:31:54 -0500

    review
    check comments

--- BuildSpring2025_ProjectRat/Assets/Scripts/Minion/MinionSpawner.cs
+++ BuildSpring2025_ProjectRat/Assets/Scripts/Minion/MinionSpawner.cs
@@ -15,7 +15,18 @@ public class MinionSpawner : MonoBehaviour
     [SerializeField] private float radius = 5f;
     [SerializeField] private float spawnDelay = 0.5f;
 
-    private List<GameObject> spawnedMinions = new List<GameObject>();
+    /*
+        - Review:
+        > add an int varaiable that determines the amount of minions that should spawn in the loop
+        > if the number is -1, the loop will run infinitely
+        > for example:
+            if the number is a 9, it should spawn 9 minions and then the spawner should stop.
+
+        - check reviews below
+        - when complete, remove the review comments
+    */
+
+    private List<GameObject> spawnedMinions = new List<GameObject>(); // review: remove the clear minions, but keep this list. also make it public
     private Coroutine spawnCoroutine;
     private int roamingCount = 0;
 
@@ -76,7 +87,7 @@ public class MinionSpawner : MonoBehaviour
             spawnCoroutine = null;
             Debug.Log("Spawner done.");
 
-            ClearMinions();
+            ClearMinions(); // review: minions should not be cleared. only the spawner should stop spawning.
         }
         else
         {
@@ -84,7 +95,7 @@ public class MinionSpawner : MonoBehaviour
         }
 
     }
-
+    // review: minions will be cleared somewhere else. this is not required. remove:
     private void ClearMinions() //Clears Minions after stopped
     {
         foreach (GameObject minion in spawnedMinions)
@@ -99,6 +110,7 @@ public class MinionSpawner : MonoBehaviour
         UpdateRoamingCount();
         Debug.Log("All minions cleared.");
     }
+    // review: add the canvas text to the scene: remember to save scene before committing
     private void UpdateRoamingCount()
     {
         if (roamingCountText != null)
