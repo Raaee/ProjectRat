7ef1ebe87f63e8b5616d2a90e56ca3781516f1b7
Paola Arraut <paoarraut05@gmail.com>
2025-03-04 23:14:18 -0500

    Update
    -Fixed the spawner and any needed changes

--- BuildSpring2025_ProjectRat/Assets/Scripts/Minion/MinionSpawner.cs
+++ BuildSpring2025_ProjectRat/Assets/Scripts/Minion/MinionSpawner.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 //main
@@ -14,21 +15,21 @@ public class MinionSpawner : MonoBehaviour
 
     [SerializeField] private float radius = 5f;
     [SerializeField] private float spawnDelay = 0.5f;
+    [SerializeField] private int spawnMax = 50;
+    [SerializeField] private bool isSpawning = false;
+    
 
-    /*
-        - Review:
-        > add an int varaiable that determines the amount of minions that should spawn in the loop
-        > if the number is -1, the loop will run infinitely
-        > for example:
-            if the number is a 9, it should spawn 9 minions and then the spawner should stop.
 
-        - check reviews below
-        - when complete, remove the review comments
-    */
+    public List<GameObject> spawnedMinions = new List<GameObject>(); 
 
-    private List<GameObject> spawnedMinions = new List<GameObject>(); // review: remove the clear minions, but keep this list. also make it public
-    private Coroutine spawnCoroutine;
+
     private int roamingCount = 0;
+
+    private void Start() 
+    {
+        StartSpawner();
+    }
+   
 
     Vector2 MinionRadius() //establishes the radius around the target
     {
@@ -49,68 +50,38 @@ public class MinionSpawner : MonoBehaviour
     }
     private IEnumerator MinionIntervals() //Courintine timer for spawner
     {
-
-        while (true)
+        while (isSpawning)
         {
-            MinionSpawn();
-            yield return new WaitForSeconds(spawnDelay);
+
+            if (spawnMax == -1 || roamingCount < spawnMax) //logic for spawn count
+
+            {
+                MinionSpawn();
+                yield return new WaitForSeconds(spawnDelay);
+            }
+
         }
+
     }
 
-    /*private void Start() //commented for testing purposes
-    {
-        StartCoroutine(MinionIntervals());
-    }*/
 
 
     //Debug Function
     [ProButton]
     private void StartSpawner()
     {
-        if (spawnCoroutine == null)
-        {
-            spawnCoroutine = StartCoroutine(MinionIntervals());
-            Debug.Log("Spawner Running");
-        }
-        else
-        {
-            Debug.Log("Error it can't Start");
-        }
+        isSpawning = true;
+        StartCoroutine(MinionIntervals());
+ 
 
     }
     [ProButton]
     private void StopSpawner()
     {
-        if (spawnCoroutine != null)
-        {
-            StopCoroutine(spawnCoroutine);
-            spawnCoroutine = null;
-            Debug.Log("Spawner done.");
-
-            ClearMinions(); // review: minions should not be cleared. only the spawner should stop spawning.
-        }
-        else
-        {
-            Debug.LogWarning("Spawner can't run");
-        }
+        isSpawning = false;
 
     }
-    // review: minions will be cleared somewhere else. this is not required. remove:
-    private void ClearMinions() //Clears Minions after stopped
-    {
-        foreach (GameObject minion in spawnedMinions)
-        {
-            if (minion != null)
-            {
-                Destroy(minion);
-            }
-        }
-        spawnedMinions.Clear();
-        roamingCount = 0;
-        UpdateRoamingCount();
-        Debug.Log("All minions cleared.");
-    }
-    // review: add the canvas text to the scene: remember to save scene before committing
+  
     private void UpdateRoamingCount()
     {
         if (roamingCountText != null)
