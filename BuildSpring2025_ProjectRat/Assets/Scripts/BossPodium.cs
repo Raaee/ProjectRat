30604d848aacbf4faba16a1b21e3ba8c0fefd729
Enzo Falone <enzofalone123@gmail.com>
2025-03-18 19:06:37 -0400

    added boss health ui bar

--- BuildSpring2025_ProjectRat/Assets/Scripts/BossPodium.cs
+++ BuildSpring2025_ProjectRat/Assets/Scripts/BossPodium.cs
@@ -1,20 +1,40 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BossPodium : MonoBehaviour
 {
+    [HideInInspector] public UnityEvent<GameObject, Color> OnBossSpawn;
     private Health health;
+    private CapsuleCollider2D podiumCollider;
+    [SerializeField] private int destroySeconds = 2;
     [SerializeField] private GameObject Boss;
 
     void Start()
     {
+        podiumCollider = GetComponent<CapsuleCollider2D>();
         health = GetComponent<Health>();
         health.OnDeath.AddListener(SpawnBoss);
     }
+
     private void SpawnBoss(GameObject go)
     {
-        Instantiate(Boss);
+        podiumCollider.enabled = false;
+        GameObject newBoss = Instantiate(Boss);
+
+        OnBossSpawn.Invoke(newBoss, GetBossColor());
+
+        StartCoroutine(DestroySelf());
     }
 
+    public Color GetBossColor()
+    {
+        return Boss.GetComponentInChildren<SpriteRenderer>().sharedMaterial.GetColor("_Outline_Color");
+    }
+
+    private IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(destroySeconds);
+        Destroy(gameObject);
+    }
 }
